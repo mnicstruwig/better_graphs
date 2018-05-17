0cb982462a81d9731c4ecca10559aee13bbb5f0e
Michael Struwig <turnthescrewdriver@gmail.com>
2018-05-17 13:28:58 +0200

    Added control for specifying which old ticks to discard

--- better_graphs/defaults.py
+++ better_graphs/defaults.py
@@ -18,6 +18,10 @@ def set_pretty_defaults(palette='Set1', context='paper', font_scale=2, line_widt
     line_width : float, optional
         Line width to use when drawing plots.
     """
+    
+    tex_font = 'cm'
+    if 'tex_font' in kwargs:
+        tex_font = kwargs['tex_font']
 
     # Set Seaborn template
     sns.set_style('ticks')
@@ -35,6 +39,6 @@ def set_pretty_defaults(palette='Set1', context='paper', font_scale=2, line_widt
     # Enable LaTeX font rendering
     if tex:
         plt.rc('text', usetex=True)
-        plt.rc('font', serif='cm')
+        plt.rc('font', serif=tex_font)
         plt.rc('font', family='serif')
         plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
